4e0b3cf665e46d0dd6ad993e42044f33a8d89968
nikuz <micurino@gmail.com>
2016-07-15 09:37:53 +0300

    Release prepare fixes

--- app/js/store.js
+++ app/js/store.js
@@ -11,7 +11,7 @@ const store = createStore(reducers, {
 });
 
 store.subscribe(function() {
-  if (window.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV !== 'production') {
     console.log(store.getState());
   }
 });
